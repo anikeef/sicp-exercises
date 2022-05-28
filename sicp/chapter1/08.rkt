fa66a50d587d041cc3df5fcc926b29b57a720e44
Gennadiy <genaydzhan70@gmail.com>
2022-05-28 15:35:10 +0600

    Fix indentation in 1.8

--- sicp/chapter1/08.rkt
+++ sicp/chapter1/08.rkt
@@ -7,15 +7,19 @@
         (/ x (square guess)))
      3))
 
+(define (diff x y) (abs (- x y)))
+
 (define (good-enough? previous-guess guess)
-  (< (/ (abs (- previous-guess guess)) guess) 0.001))
+  (< (/ (diff previous-guess guess)
+        guess)
+     0.001))
 
 (define (cubic-root-iter previous-guess guess x)
   (if (good-enough? previous-guess guess)
-     guess
-     (cubic-root-iter guess
-                (improve guess x)
-                x)))
+      guess
+      (cubic-root-iter guess
+                       (improve guess x)
+                       x)))
 
 (define (cubic-root x)
   (cubic-root-iter 1.0 (improve 1.0 x) x))
