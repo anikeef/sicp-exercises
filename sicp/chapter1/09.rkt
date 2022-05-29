24bef5713a21122ed41b6d768c9dd09e02283845
Gennadiy <genaydzhan70@gmail.com>
2022-05-29 11:22:33 +0600

    Solve 1.9

---
+++ sicp/chapter1/09.rkt
@@ -0,0 +1,35 @@
+#lang sicp
+
+; Recursive process
+(define (plus1 a b)
+  (if (= a 0)
+      b
+      (inc (plus1 (dec a) b))))
+
+#|
+(plus1 4 5)
+(inc (plus1 3 5))
+(inc (inc (plus1 2 5)))
+(inc (inc (inc (plus1 1 5))))
+(inc (inc (inc (inc (plus1 0 5)))))
+(inc (inc (inc (inc 5))))
+(inc (inc (inc 6)))
+(inc (inc 7))
+(inc 8)
+9
+|#
+
+; Iterative process
+(define (plus2 a b)
+  (if (= a 0)
+      b
+      (plus2 (dec a) (inc b))))
+
+#|
+(plus2 4 5)
+(plus2 3 6)
+(plus2 2 7)
+(plus2 1 8)
+(plus2 0 9)
+9
+|#
