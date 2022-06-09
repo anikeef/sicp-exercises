2cf9779b1ac7fcd08d1c48ac9013cee3333b5da3
Gennadiy <genaydzhan70@gmail.com>
2022-06-09 18:22:55 +0600

    Solve 2.6

---
+++ sicp/chapter2/06.rkt
@@ -0,0 +1,11 @@
+#lang sicp
+
+(define zero (lambda (f) (lambda (x) x)))
+
+(define (add-1 n)
+  (lambda (f) (lambda (x) (f ((n f) x)))))
+
+(define one (lambda (f) (lambda (x) (f x))))
+
+(define (add m n)
+  (lambda (f) (lambda (x) ((n f) ((m f) x)))))
