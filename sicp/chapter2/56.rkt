df1bf1a89ed8800d439c531f6562c04be7818236
Gennadiy <genaydzhan70@gmail.com>
2022-06-15 20:21:25 +0600

    Solve 2.56

---
+++ sicp/chapter2/56.rkt
@@ -0,0 +1,62 @@
+#lang sicp
+
+(define constant? number?)
+
+(define variable? symbol?)
+
+(define same-variable? eq?)
+
+(define (=number? exp num)
+  (and (number? exp) (= exp num)))
+
+(define (sum? exp)
+  (and (pair? exp) (eq? (car exp) '+)))
+
+(define addend cadr)
+(define (augend exp)
+  (if (null? (cdddr exp))
+      (caddr exp)
+      (append '(+) (cddr exp))))
+
+(define (make-sum a1 a2)
+  (cond ((=number? a1 0) a2)
+        ((=number? a2 0) a1)
+        ((and (number? a1) (number? a2)) (+ a1 a2))
+        (else (list '+ a1 a2))))
+
+(define (product? exp)
+  (and (pair? exp) (eq? (car exp) '*)))
+
+(define multiplier cadr)
+(define (multiplicand exp)
+  (if (null? (cdddr exp))
+      (caddr exp)
+      (append '(*) (cddr exp))))
+
+(define (make-product m1 m2)
+  (cond ((or (=number? m1 0) (=number? m2 0)) 0)
+        ((=number? m1 1) m2)
+        ((=number? m2 1) m1)
+        ((and (number? m1) (number? m2)) (* m1 m2))
+        (else (list '* m1 m2))))
+
+; Solution
+
+(define (deriv exp var)
+  (cond ((constant? exp) 0)
+        ((variable? exp) 
+          (if (same-variable? exp var) 1 0))
+        ((sum? exp)
+          (make-sum (deriv (addend exp) var)
+                    (deriv (augend exp) var)))
+        ((product? exp)
+          (make-sum
+            (make-product (multiplier exp)
+                          (deriv (multiplicand exp) var))
+            (make-product (deriv (multiplier exp) var)
+                          (multiplicand exp))))
+        (else
+          (error "неизвестный тип выражения -- DERIV" exp))))
+
+(display (deriv '(* x x x) 'x))
+(newline)
